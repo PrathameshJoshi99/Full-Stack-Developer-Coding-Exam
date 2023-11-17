d15c7d9f6a5996320c1cb70211f6f49a0c2b42ab
Prathamesh Joshi <118970122+PrathameshJoshi99@users.noreply.github.com>
2023-11-17 20:18:15 +0530

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,9 @@
+# notes/urls.py
+
+from django.urls import path
+from .views import NoteListCreateView, NoteRetrieveUpdateDestroyView
+
+urlpatterns = [
+    path('notes/', NoteListCreateView.as_view(), name='note-list-create'),
+    path('notes/<int:pk>/', NoteRetrieveUpdateDestroyView.as_view(), name='note-retrieve-update-destroy'),
+]
