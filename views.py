916b9aa9e279107cc1d3c6ac07879db4e7014a5c
Prathamesh Joshi <118970122+PrathameshJoshi99@users.noreply.github.com>
2023-11-17 20:17:54 +0530

    Create views.py

---
+++ views.py
@@ -0,0 +1,13 @@
+# notes/views.py
+
+from rest_framework import generics
+from .models import Note
+from .serializers import NoteSerializer
+
+class NoteListCreateView(generics.ListCreateAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
+class NoteRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
